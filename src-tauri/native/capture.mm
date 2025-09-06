4cd7af7a8eedda2bd62fa9cb2f0bc7203ef4d794
Patryk Sztuczka <patryk.sztuczka00@gmail.com>
2025-09-06 13:17:09 +0200

    add mic capturing

--- src-tauri/native/capture.mm
+++ src-tauri/native/capture.mm
@@ -16,6 +16,9 @@ static std::atomic<bool> g_isCapturing(false);
 static SCStream *g_stream = nil;
 static ExtAudioFileRef g_audioFile = NULL;
 static std::atomic<bool> g_clientFormatSet(false);
+static std::atomic<bool> g_micClientFormatSet(false);
+static AVAudioEngine *g_micEngine = nil;
+static ExtAudioFileRef g_micFile = NULL;
 
 static char *dup_cstr(const char *s) {
   if (!s) {
@@ -162,6 +165,54 @@ static BOOL ensureAudioFileAtPath(NSString *path, NSError **error) {
   OSStatus st =
       ExtAudioFileCreateWithURL(url, kAudioFileWAVEType, &asbd, NULL,
                                 kAudioFileFlags_EraseFile, &g_audioFile);
+  if (st != noErr) {
+    if (error)
+      *error = [NSError errorWithDomain:NSOSStatusErrorDomain
+                                   code:st
+                               userInfo:nil];
+    return NO;
+  }
+  return YES;
+}
+
+static BOOL ensureAudioFileAtPathWithChannels(NSString *path, double sampleRate,
+                                              UInt32 channels,
+                                              NSError **error) {
+  AudioStreamBasicDescription asbd = {0};
+  asbd.mSampleRate = sampleRate;
+  asbd.mFormatID = kAudioFormatLinearPCM;
+  asbd.mFormatFlags =
+      kLinearPCMFormatFlagIsSignedInteger | kLinearPCMFormatFlagIsPacked;
+  asbd.mFramesPerPacket = 1;
+  asbd.mChannelsPerFrame = channels;
+  asbd.mBitsPerChannel = 16;
+  asbd.mBytesPerFrame = (asbd.mBitsPerChannel / 8) * asbd.mChannelsPerFrame;
+  asbd.mBytesPerPacket = asbd.mBytesPerFrame * asbd.mFramesPerPacket;
+
+  // Ensure parent directory exists
+  NSString *parent = [path stringByDeletingLastPathComponent];
+  if (parent.length > 0) {
+    NSFileManager *fm = [NSFileManager defaultManager];
+    BOOL isDir = NO;
+    BOOL exists = [fm fileExistsAtPath:parent isDirectory:&isDir];
+    if (!exists || !isDir) {
+      NSError *mkErr = nil;
+      BOOL ok = [fm createDirectoryAtPath:parent
+              withIntermediateDirectories:YES
+                               attributes:nil
+                                    error:&mkErr];
+      if (!ok) {
+        if (error)
+          *error = mkErr;
+        return NO;
+      }
+    }
+  }
+
+  CFURLRef url = (__bridge CFURLRef)[NSURL fileURLWithPath:path];
+  OSStatus st =
+      ExtAudioFileCreateWithURL(url, kAudioFileWAVEType, &asbd, NULL,
+                                kAudioFileFlags_EraseFile, &g_micFile);
   if (st != noErr) {
     if (error)
       *error = [NSError errorWithDomain:NSOSStatusErrorDomain
@@ -374,6 +425,71 @@ bool sc_start_capture(const char *kind, const char *id, const char *out_path,
       }
     }];
 
+    // Start microphone capture to separate file: same base + "-mic.wav"
+    @try {
+      // Request permission if needed
+      AVAuthorizationStatus auth =
+          [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeAudio];
+      if (auth == AVAuthorizationStatusNotDetermined) {
+        dispatch_semaphore_t s = dispatch_semaphore_create(0);
+        [AVCaptureDevice requestAccessForMediaType:AVMediaTypeAudio
+                                 completionHandler:^(BOOL granted) {
+                                   dispatch_semaphore_signal(s);
+                                 }];
+        dispatch_semaphore_wait(s, DISPATCH_TIME_FOREVER);
+        auth =
+            [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeAudio];
+      }
+      if (auth != AVAuthorizationStatusAuthorized) {
+        NSLog(@"Mic access not authorized; skipping mic capture");
+      } else {
+        g_micEngine = [AVAudioEngine new];
+        AVAudioInputNode *input = g_micEngine.inputNode;
+        AVAudioFormat *inFormat = [input inputFormatForBus:0];
+        NSString *baseNoExt = [path stringByDeletingPathExtension];
+        NSString *micPath = [baseNoExt stringByAppendingString:@"-mic.wav"];
+        NSError *micErr = nil;
+        UInt32 micChannels = (UInt32)inFormat.channelCount;
+        if (!ensureAudioFileAtPathWithChannels(micPath, 48000.0, micChannels,
+                                               &micErr)) {
+          NSLog(@"mic file error: %@", micErr.localizedDescription);
+        } else {
+          const AudioStreamBasicDescription *inAsbd =
+              inFormat.streamDescription;
+          OSStatus ps = ExtAudioFileSetProperty(
+              g_micFile, kExtAudioFileProperty_ClientDataFormat,
+              sizeof(AudioStreamBasicDescription), inAsbd);
+          if (ps != noErr) {
+            NSLog(@"ExtAudioFileSetProperty(ClientDataFormat mic) failed: %d",
+                  (int)ps);
+          }
+          [input
+              installTapOnBus:0
+                   bufferSize:1024
+                       format:inFormat
+                        block:^(AVAudioPCMBuffer *buffer, AVAudioTime *when) {
+                          if (!g_micFile)
+                            return;
+                          OSStatus ws = ExtAudioFileWrite(
+                              g_micFile, (UInt32)buffer.frameLength,
+                              buffer.audioBufferList);
+                          if (ws != noErr) {
+                            NSLog(@"ExtAudioFileWrite mic failed: %d", (int)ws);
+                          }
+                        }];
+          NSError *startErr = nil;
+          [g_micEngine prepare];
+          if (![g_micEngine startAndReturnError:&startErr]) {
+            NSLog(@"mic engine start error: %@", startErr.localizedDescription);
+          } else {
+            NSLog(@"Mic capture started");
+          }
+        }
+      }
+    } @catch (NSException *ex) {
+      NSLog(@"Mic start exception: %@", ex.reason);
+    }
+
     g_isCapturing.store(true);
     return true;
   }
@@ -393,6 +509,17 @@ void sc_stop_capture() {
     if (g_audioFile) {
       ExtAudioFileDispose(g_audioFile);
       g_audioFile = NULL;
+    }
+    if (g_micEngine) {
+      AVAudioInputNode *input = g_micEngine.inputNode;
+      [input removeTapOnBus:0];
+      [g_micEngine stop];
+      g_micEngine = nil;
+      NSLog(@"Mic capture stopped");
+    }
+    if (g_micFile) {
+      ExtAudioFileDispose(g_micFile);
+      g_micFile = NULL;
     }
     g_writer = nil;
     g_clientFormatSet.store(false);
